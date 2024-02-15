f65217b1a2c1a757ff237ab7d45c4820c82d7787
goxjanskloon <goxjanskloon@outlook.com>
2024-02-15 22:27:51 +0800

    first commit

---
+++ BoardFrame.java
@@ -0,0 +1,194 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import github.goxjanskloon.logicblocks.Board;
+import github.goxjanskloon.logicblocks.Board.Block;
+public class BoardFrame extends JFrame{
+    private class BlockModifyListener implements Board.ModifyListener<BlockModifyListener>{
+        private static AtomicInteger count=new AtomicInteger(0);
+        public final int id=count.getAndIncrement();
+        public int compareTo(BlockModifyListener ml){return Integer.valueOf(id).compareTo(ml.id);}
+        public void modifyBlock(Block block){
+            int xl=-xOffset/blockSize,yl=-yOffset/blockSize,xr=xl+getWidth()/blockSize,yr=yl+getHeight()/blockSize;
+            if(xl<0) xl=0;if(yl<0) yl=0;if(xr>=board.getWidth()) xr=board.getWidth()-1;if(yr>=board.getHeight()) yr=board.getHeight()-1;
+            if(block.x<xl||xr<block.x||block.y<yl||yr<block.y) return;
+            paint(block);
+    }}
+    private enum OperationType{CLEAR,SET_TYPE,NONE};
+    private static BufferedImage[][] images=null;
+    private Board board;
+    private Graphics graphics=null;
+    private int xOffset=0,yOffset=0,blockSize=50,choosedType=0,xOfsOrg=0,yOfsOrg=0;
+    private File file=null;
+    private OperationType operationType=OperationType.NONE;
+    private Block choosedBlock=null;
+    private boolean MMoved=false;
+    static{try{
+        images=new BufferedImage[][]{
+        {ImageIO.read(new File("img/VOID0.png")),
+         ImageIO.read(new File("img/VOID1.png"))},
+        {ImageIO.read(new File("img/OR0.png")),
+         ImageIO.read(new File("img/OR1.png"))},
+        {ImageIO.read(new File("img/NOT0.png")),
+         ImageIO.read(new File("img/NOT1.png"))},
+        {ImageIO.read(new File("img/AND0.png")),
+         ImageIO.read(new File("img/AND1.png"))},
+        {ImageIO.read(new File("img/XOR0.png")),
+         ImageIO.read(new File("img/XOR1.png"))},
+        {ImageIO.read(new File("img/SRC0.png")),
+         ImageIO.read(new File("img/SRC1.png"))}
+        };}catch(IOException e){e.printStackTrace();}}
+    public BoardFrame(){
+        super();
+        this.board=new Board();
+        board.addModifyListener(new BlockModifyListener());
+        addKeyListener(new KeyAdapter(){
+            public void keyPressed(KeyEvent ke){
+                if(!ke.isControlDown()) return;
+                switch(ke.getKeyCode()){
+                case'N':{
+                    Scanner s=new Scanner(System.in);
+                    board.resetWithSize(s.nextInt(),s.nextInt());
+                    s.close();repaint();}break;
+                case'S':
+                    if(ke.isShiftDown()?saveAsFile():saveFile()) JOptionPane.showMessageDialog(BoardFrame.this,"Save failed!");
+                    repaint();break;
+                case'O':
+                    if(openFile()) JOptionPane.showMessageDialog(BoardFrame.this,"Open failed!");
+                    repaint();break;
+                case'L':operationType=OperationType.LINK;break;
+                case'Q':blockSize+=10;repaint();break;
+                case'E':if(blockSize>10)blockSize-=10;repaint();break;
+                default:{char c=ke.getKeyChar();if('1'<=c&&c<='6') choosedType=c-'1';}break;
+            }}
+        });
+        addMouseListener(new MouseAdapter(){
+            public void mousePressed(MouseEvent me){if(!board.isEmpty()){
+                switch(me.getButton()){
+                case MouseEvent.BUTTON1:
+                    xOfsOrg=xOffset-me.getX();yOfsOrg=yOffset-me.getY();
+                    operationType=OperationType.CLEAR;break;
+                case MouseEvent.BUTTON3:operationType=OperationType.SET_TYPE;break;
+                default:break;
+            }}}
+            public void mouseReleased(MouseEvent me){
+                switch(operationType){
+                case LINK:{
+                    Board.Block block=MToBlock(me.getX(),me.getY());
+                    if(block!=null)
+                        if(choosedBlock==null) choosedBlock=block;
+                        else{choosedBlock.addOutput(block);operationType=OperationType.NONE;choosedBlock=null;}
+                    }break;
+                case CLEAR:if(!MMoved){
+                    Board.Block block=MToBlock(me.getX(),me.getY());
+                    if(block!=null) block.clear();
+                    }operationType=OperationType.NONE;MMoved=false;break;
+                case SET_TYPE:if(!MMoved){
+                    Board.Block block=MToBlock(me.getX(),me.getY());
+                    if(block!=null)
+                        if(block.getType()!=Block.Type.SRC) block.setType(Block.Type.valueOf(choosedType));
+                        else block.inverseValue();
+                    }operationType=OperationType.NONE;MMoved=false;break;
+                case NONE:break;
+                default:operationType=OperationType.NONE;break;
+        }}});
+        addMouseMotionListener(new MouseAdapter(){
+            public void mouseDragged(MouseEvent me){
+                xOffset=me.getX()+xOfsOrg;yOffset=me.getY()+yOfsOrg;
+                repaint();MMoved=true;
+            }});
+    }
+    @Override
+    public void setVisible(boolean visible){
+        super.setVisible(visible);
+        if(visible) graphics=getGraphics();
+    }
+    private void paint(Block block,Graphics g){
+        int x=block.x*blockSize+xOffset,y=block.y*blockSize+yOffset;
+        if(x<-blockSize||getWidth()<x||y<-blockSize||getHeight()<y);
+        g.drawImage(images[block.getType().ordinal()][block.getValue()?1:0],x,y,blockSize,blockSize,null);
+        for(Board.Block target:block.getOutputs()){
+            int tx=target.x*blockSize+xOffset,ty=target.y*blockSize+yOffset,mx=x+tx>>1,my=x+ty>>1;
+            g.drawLine(x,y,mx,my);
+            g.setColor(LINK_LC);
+            g.setColor(LINK_RC);
+            g.drawLine(mx,my,tx,ty);
+        }
+    }
+    private void paint(Block block){paint(block,graphics);}
+    @Override
+    public void paint(Graphics g){
+        if(board==null||board.isEmpty()){g.clearRect(getX(),getY(),getWidth(),getHeight());return;};
+        Image image=createImage(getWidth(),getHeight());
+        Graphics ig=image.getGraphics();
+        int xl=-xOffset/blockSize,yl=-yOffset/blockSize,xr=xl+getWidth()/blockSize,yr=yl+getHeight()/blockSize;
+        if(xl<0) xl=0;if(yl<0) yl=0;if(xr>=board.getWidth()) xr=board.getWidth()-1;if(yr>=board.getHeight()) yr=board.getHeight()-1;
+        for(int i=yl;i<=yr;i++)
+            for(int j=xl;j<=xr;j++) paint(board.get(j,i),ig);
+        g.drawImage(image,0,0,null);
+    }
+    private Board.Block MToBlock(int x,int y){
+        x-=xOffset;y-=yOffset;
+        x/=blockSize;y/=blockSize;
+        if(x<0||board.getWidth()<=x||y<0||board.getHeight()<=y) return null;
+        return board.get(x,y);
+    }
+    private boolean openFile(){
+        JFileChooser fc=new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if(fc.showOpenDialog(fc)!=JFileChooser.APPROVE_OPTION){return false;}
+        file=fc.getSelectedFile();
+        FileReader reader=null;
+        try{reader=new FileReader(file);
+        }catch(FileNotFoundException e){e.printStackTrace();return false;}
+        if(board.loadFrom(reader)){
+            try{reader.close();
+            }catch(IOException e){e.printStackTrace();return false;}
+            return true;
+        }try{reader.close();
+        }catch(IOException e){e.printStackTrace();return false;}
+        return false;
+    }
+    private boolean saveFile(){
+        if(file==null) return saveAsFile();
+        FileWriter writer=null;
+        try{writer=new FileWriter(file);
+        }catch(IOException e){e.printStackTrace();return false;}
+        if(board.exportTo(writer)){
+            try{writer.close();
+            }catch(IOException e){e.printStackTrace();return false;}
+            return true;
+        }return false;
+    }
+    private boolean saveAsFile(){
+        JFileChooser fc=new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if(fc.showOpenDialog(fc)!=JFileChooser.APPROVE_OPTION){return false;}
+        file=fc.getSelectedFile();
+        return saveFile();
+    }
+    public static void main(String[] args){
+        BoardFrame bf=new BoardFrame();
+        bf.setTitle("LogicCircuits");
+        bf.setSize(1000,600);
+        bf.setBackground(Color.WHITE);
+        bf.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        bf.setVisible(true);
+    }
+}
---
+++ github/goxjanskloon/logicblocks/Board.java
@@ -0,0 +1,163 @@
+package github.goxjanskloon.logicblocks;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+public class Board{
+    public interface ModifyListener<T extends ModifyListener<T>> extends Comparable<T>{void modifyBlock(Block block);}
+    public class Block implements Comparable<Block>{
+        private static final int[][] POS_OFF={{1,0,1},{-1,0,0},{0,-1,3},{0,1,2}};
+        public enum Type{
+            VOID(),LINE(),NOT(),AND(),XOR(),SRC();
+            public static Type valueOf(int value){
+                switch(value){
+                case 0:return VOID;
+                case 1:return LINE;
+                case 2:return NOT;
+                case 3:return AND;
+                case 4:return XOR;
+                case 5:return SRC;
+                default:return null;
+        }}}
+        public final int x,y;
+        private AtomicInteger type,facing;
+        private AtomicBoolean value;
+        private Block(Type type,boolean value,int x,int y){
+            this.x=x;this.y=y;
+            this.type=new AtomicInteger(type.ordinal());
+            this.facing=new AtomicInteger(0);
+            this.value=new AtomicBoolean(value);
+        }
+        public Type getType(){return Type.valueOf(type.get());}
+        public boolean setType(Type type){
+            if(getType()==type) return false;
+            this.type.set(type.ordinal());
+            flush();callModifyListeners(this);
+            return true;
+        }
+        public int getFacing(){return facing.get();}
+        public int toNextFacing(){
+            int c=getFacing();
+            facing.set(c==3?c=0:++c);
+            return c;
+        }
+        public boolean getValue(){return value.get();}
+        public boolean inverseValue(){
+            if(getType()!=Type.SRC) throw new UnsupportedOperationException("Calling inverseValue() on a not SRC-Type Block");
+            boolean result=!value.getAndSet(!getValue());
+            callModifyListeners(this);flushOutputs();
+            return result;
+        }
+        private void flushOutputs(){
+            if(getType()==Type.LINE){
+                int f=getFacing();
+                for(int i=0;i<4;i++){
+                    if(i==POS_OFF[f][2]) continue;
+                    int[] p=POS_OFF[i];
+                    int bx=x+p[0],by=y+p[1];
+                    if(bx<0||getWidth()<=bx||by<0||getHeight()<=by) continue;
+                    Block b=get(bx,by);
+                    if(b.getType()==Type.LINE&&b.getFacing()==i) b.flush();
+            }}else for(int i=0;i<4;i++){
+                int[] p=POS_OFF[i];
+                int bx=x+p[0],by=y+p[1];
+                if(bx<0||getWidth()<=bx||by<0||getHeight()<=by) continue;
+                Block b=get(bx,by);
+                if(b.getType()==Type.LINE&&b.getFacing()==i) b.flush();
+        }}
+        public void flush(){
+            threadPool.execute(new Runnable(){public void run(){
+            Type t=getType();
+            if(t==Type.VOID||t==Type.SRC) return;
+            boolean newValue=false;
+            ArrayList<Block> in=null;
+            int f=0;
+            if(t!=Type.LINE){
+                in=new ArrayList<Block>();
+                for(int[] p:POS_OFF){
+                int bx=x+p[0],by=y+p[1];
+                if(bx<0||getWidth()<=bx||by<0||getHeight()<=by) continue;
+                Block b=get(bx,by);
+                if(b.getType()==Type.LINE&&b.getFacing()==p[3]) in.add(b);
+            }}else f=getFacing();
+            switch(getType()){
+            case LINE:{
+                int[] p=POS_OFF[POS_OFF[f][3]];
+                int bx=x+p[0],by=y+p[1];
+                if(bx<0||getWidth()<=bx||by<0||getHeight()<=by) break;
+                newValue=get(bx,by).getValue();}break;
+            case NOT:newValue=in.size()==1&&!in.get(1).getValue();break;
+            case AND:newValue=in.size()==2&&in.get(1).getValue()&&in.get(2).getValue();break;
+            case XOR:newValue=in.size()==2&&in.get(1).getValue()^in.get(2).getValue();break;
+            case SRC:newValue=getValue();break;
+            default:break;}
+            if(value.compareAndSet(!newValue,newValue)){
+                flushOutputs();
+                callModifyListeners(Block.this);
+        }}});}
+        public boolean clear(){
+            if(isEmpty()) return false;
+            setType(Type.VOID);callModifyListeners(this);
+            return true;
+        }
+        public int compareTo(Block block){return Integer.valueOf(y*getHeight()+x).compareTo(block.y*getHeight()+block.x);}
+    }
+    private ArrayList<ArrayList<Block>> blocks=new ArrayList<ArrayList<Block>>();
+    private ConcurrentSkipListSet<ModifyListener<?>> modifyListeners=new ConcurrentSkipListSet<ModifyListener<?>>();
+    private ExecutorService threadPool=Executors.newCachedThreadPool(new ThreadFactory(){
+        public Thread newThread(Runnable r){Thread thread=new Thread(r);thread.setDaemon(true);return thread;}});
+    public Board(){}
+    public Board(int width,int height){resetWithSize(width, height);}
+    public boolean addModifyListener(ModifyListener<?> modifyListener){return modifyListeners.add(modifyListener);}
+    public boolean removeModifyListener(ModifyListener<?> modifyListener){return modifyListeners.remove(modifyListener);}
+    public boolean clearModifyListeners(){
+        if(modifyListeners.isEmpty()) return false;
+        modifyListeners.clear();return true;
+    }
+    private void callModifyListeners(Block block){for(ModifyListener<?> ml:modifyListeners) ml.modifyBlock(block);}
+    public Block get(int x,int y){return blocks.get(x).get(y);}
+    public boolean isEmpty(){return blocks.isEmpty();}
+    public int getWidth(){return blocks.size();}
+    public int getHeight(){return isEmpty()?0:blocks.getFirst().size();}
+    public boolean clear(){
+        if(isEmpty()) return false;
+        silence();blocks.clear();
+        return true;
+    }
+    public void silence(){threadPool.shutdownNow();}
+    public boolean loadFrom(Readable reader){
+        clear();try{
+        Scanner scanner=new Scanner(reader);
+        int width=scanner.nextInt(),height=scanner.nextInt();
+        for(int i=0;i<height;i++){
+            blocks.add(new ArrayList<Block>());
+            for(int j=0;j<width;j++){
+                blocks.getLast().add(new Block(Block.Type.valueOf(scanner.nextInt()),scanner.nextInt()==1,i,j));
+            }
+        }scanner.close();
+        }catch(Exception e){e.printStackTrace();clear();return false;}
+        return true;
+    }
+    public boolean exportTo(Writer writer){try{
+        writer.write(blocks.size()+" "+blocks.getFirst().size()+" ");
+        for(int i=0;i<blocks.size();i++)
+            for(int j=0;j<blocks.get(i).size();j++){
+                Block block=get(i,j);
+                writer.write(block.type.get()+" "+(block.value.get()?1:0)+" ");
+            }
+        }catch(Exception e){e.printStackTrace();return false;}
+        return true;
+    }
+    public void resetWithSize(int width,int height){
+        clear();
+        for(int i=0;i<height;i++){
+            blocks.add(new ArrayList<Block>());
+            for(int j=0;j<width;j++) blocks.getLast().add(new Block(Block.Type.VOID,false,i,j));
+        }
+    }
+}
