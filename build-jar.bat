f434c23948bf397784dcde4de5a05da880840462
goxjanskloon <goxjanskloon@outlook.com>
2024-02-18 23:12:31 +0800

    Build script modified

--- build-jar.bat
+++ build-jar.bat
@@ -3,5 +3,5 @@ cd /d %~dp0
 mkdir build
 mkdir build\bin
 javac src\BoardFrame.java -d build\bin -cp src
-xcopy src\img\ build\img\ /s/y
+xcopy src\img\ build\bin\img\ /s/y
 jar --create --file build\LogicBlocks.jar --main-class BoardFrame -C build\bin .
